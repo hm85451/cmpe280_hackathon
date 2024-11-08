68217a3ddcba51d1d33b57cacc311420317c8e6a
hm85451 <haoming.chen@sjsu.edu>
2024-11-07 20:44:13 -0800

    added side bar and country flag feature

--- hackathon-app/src/components/Header.js
+++ hackathon-app/src/components/Header.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
+import ReactFlagsSelect from "react-flags-select";
 
 function Header() {
+    const [selectedCountry, setSelectedCountry] = useState(null);
+
     return (
         <header className="header">
-            <div className="logo">Logo</div>
             <div className="title">Macroeconomic Researcher Food Security Dashboard</div>
-            <div className="country-flag">Country Flag</div>
+            <ReactFlagsSelect
+            className="react-flag-select"
+            countries={["US", "CN", "IN"]}
+            selected={selectedCountry}
+            onSelect={(code) => setSelectedCountry(code)}
+  />
         </header>
+        
     );
 }
 
--- hackathon-app/src/components/MainContent.js
+++ hackathon-app/src/components/MainContent.js
@@ -5,13 +5,6 @@ function MainContent({selectedOption}) {
     return (
         <main className="main-content">
             <div className="timeline">{selectedOption}</div>
-            <div className="charts">
-                <div className="chart">GDP Chart</div>
-                <div className="chart">FDI Outflows Chart</div>
-                <div className="chart">FDI Inflows Chart</div>
-            </div>
-            <div className="annotations">Annotation</div>
-            <div className="footer">Footer</div>
         </main>
     );
 }
